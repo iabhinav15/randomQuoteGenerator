8038dd4df5f5c64454e703bab778d87e4b00ddef
officialAbhinav <official.abhinavsingh15@gmail.com>
2024-01-26 01:49:47 +0530

    Update index.html and script.js files

--- script.js
+++ script.js
@@ -1,58 +1,41 @@
-const quote = document.getElementById('quote')
-const author = document.getElementById("author")
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
 
 const btn = document.getElementById("btn");
-// const quotes = document.getElementById('quotes')
-let i=0;
+let i = 0;
 
-btn.addEventListener("click", async ()=>{
-  getQuotes();
-  //   fetch("https://type.fit/api/quotes")
-  // .then(function(response) {
-  //     return response.json();
-  // })
-  // .then((data)=> {
-    // // console.log(data);
-    // let div = document.createElement("div");
-    // // data.map((ele)=>{ 
-      //   let p1 = document.createElement('p')
-      //   let p2 = document.createElement('p')
-      //   p1.innerText = ele.text;
-      //   p2.innerText = ele.author;
-      //   div.appendChild(p1)
-      //   div.appendChild(p2)
-      // // })
-      // quotes.innerHTML = div.innerHTML;
-      
-      
-
-    // }
-       
-  // )
+btn.addEventListener("click", async () => {
+  await getQuotes();
   myFun();
-  
 })
 
-const getQuotes = async () =>{
-  const response = await fetch("https://type.fit/api/quotes")
+const getQuotes = async () => {
+  const response = await fetch("https://type.fit/api/quotes");
   const data = await response.json();
-  quote.innerHTML=data[i].text;
-  author.innerHTML=data[i].author;
+  // console.log(data);
+  if(i == data.length){
+    i = 0;
+  }
+  quote.innerHTML = data[i].text;
+  author.innerHTML = data[i].author;
   i++;
 }
-
 getQuotes();
 
-// const buton=document.getElementById("btn");
-const allchar="0123456789ABCDEF";
-
-// buton.addEventListener("click",myFun);
-
-function myFun(){
-  var  randcol= "";
-for(var i=0; i<6; i++){
-   randcol += allchar[Math.floor(Math.random()*16)];
-}
-document.body.style.backgroundColor= "#"+randcol;
+const allchar = "0123456789ABCDEF";
+function myFun() {
+  let randcol = "";
+  for (let i = 0; i < 6; i++) {
+    randcol += allchar[Math.floor(Math.random() * 16)];
+  }
+  document.body.style.backgroundColor = "#" + randcol;
 }
 myFun();
+
+const copybtn = document.getElementById("copybtn");
+
+copybtn.addEventListener("click",  () =>{
+  let quote = document.getElementById("quote");
+  navigator.clipboard.writeText(quote.innerHTML);
+  alert("Copied the text: " + quote.innerHTML);
+})
